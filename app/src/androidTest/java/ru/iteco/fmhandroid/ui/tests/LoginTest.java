62628d3a872e62bd5e0f18474c516eeba3967e52
NikitaBez <bezmenov.nikita@gmail.com>
2024-08-05 20:16:25 +0900

    Update LoginTest (rename class, rename tests)
    Изменил название класса LoginTest1 -> LoginTest
    Изменил название тестов в соответствии с требованиями
    Убрал большую часть закомментированных подсказок

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/tests/LoginTest.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/tests/LoginTest.java
@@ -2,7 +2,6 @@ package ru.iteco.fmhandroid.ui.tests;
 
 import android.view.View;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -29,7 +28,7 @@ import static org.hamcrest.Matchers.not;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginTest1 {
+public class LoginTest {
 
     private LoginSteps loginSteps;
     private NewsPage newsPage;
@@ -48,24 +47,19 @@ public class LoginTest1 {
 
         mActivityScenarioRule.getScenario().onActivity(activity -> decorView = activity.getWindow().getDecorView());
 
-
         // Проверка состояния входа и выход из приложения, если вход выполнен
         logged.ensureLoggedOut();
     }
 
     @Test
-    public void loginTest1() { //Проверка входа в систему с валидными значениями логина и пароля
-
+    public void validLoginAndPassword() {
         // Получаем валидные данные для логина
         DataHelper validCredentials = DataHelper.validCredentials();
-
         // Логинимся
         loginSteps.login(validCredentials);
-
         // Проверка открытия главной страницы с блоком новостей
         newsPage.waitForElement(R.id.all_news_text_view, 5000);
         newsPage.verifyNewsSection();
-
         // Разлогинивание
         newsPage.clickAuthorizationButton();
         newsPage.waitForElement(android.R.id.title, 5000);
@@ -73,59 +67,54 @@ public class LoginTest1 {
     }
 
     @Test
-    public void invalidLoginTest() { //Проверка входа в систему с невалидным значением логина
+    public void invalidLoginAndValidPassword() {
         DataHelper invalidLogin = DataHelper.invalidLogin();
         loginSteps.invalidCredentials(invalidLogin);
-        // Проверка Toast message
         onView(withText(DataHelper.TOAST_MESSAGE_INVALID_LOGIN_AND_PASSWORD))
                 .inRoot(withDecorView(Matchers.not(decorView)))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void invalidPasswordTest() { //Проверка входа в систему с невалидным значением пароля
+    public void validLoginAndInvalidPassword() {
         DataHelper invalidPassword = DataHelper.invalidPassword();
         loginSteps.invalidCredentials(invalidPassword);
-        // Проверка Toast message
         onView(withText(DataHelper.TOAST_MESSAGE_INVALID_LOGIN_AND_PASSWORD))
                 .inRoot(withDecorView(Matchers.not(decorView)))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void invalidCredentialsTest() { //Проверка входа в систему с невалидными значениями логина и пароля
+    public void invalidLoginAndInvalidPassword() {
         DataHelper invalidCredentials = DataHelper.invalidCredentials();
         loginSteps.invalidCredentials(invalidCredentials);
-        // Проверка Toast message
         onView(withText(DataHelper.TOAST_MESSAGE_INVALID_LOGIN_AND_PASSWORD))
                 .inRoot(withDecorView(Matchers.not(decorView)))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void emptyLogin() { //Проверка входа в систему с пустым полем логина
+    public void emptyLoginAndValidPassword() {
         DataHelper emptyLogin = DataHelper.emptyLogin();
         loginSteps.invalidCredentials(emptyLogin);
-        // Проверка Toast message
-        onView(withText(DataHelper.TOAST_MESSAGE_EMPTY_DATA))
-                .inRoot(withDecorView(Matchers.not(decorView)))
-                .check(matches(isDisplayed()));
-    }
-    @Test
-    public void emptyPassword() { //Проверка входа в систему с пустым полем пароля
-        DataHelper emptyPassword = DataHelper.emptyPassword();
-        loginSteps.invalidCredentials(emptyPassword);
-        // Проверка Toast message
         onView(withText(DataHelper.TOAST_MESSAGE_EMPTY_DATA))
                 .inRoot(withDecorView(Matchers.not(decorView)))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void emptyCredentials() { //Проверка входа в систему с пустымями логина и пароля
+    public void validLoginAndEmptyPassword() {
+        DataHelper emptyPassword = DataHelper.emptyPassword();
+        loginSteps.invalidCredentials(emptyPassword);
+        onView(withText(DataHelper.TOAST_MESSAGE_EMPTY_DATA))
+                .inRoot(withDecorView(Matchers.not(decorView)))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void EmptyLoginAndEmptyPassword() {
         DataHelper emptyCredentials = DataHelper.emptyCredentials();
         loginSteps.invalidCredentials(emptyCredentials);
-        // Проверка Toast message
         onView(withText(DataHelper.TOAST_MESSAGE_EMPTY_DATA))
                 .inRoot(withDecorView(Matchers.not(decorView)))
                 .check(matches(isDisplayed()));
